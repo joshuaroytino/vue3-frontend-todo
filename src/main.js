338398fe44e2e2c73ddf1c455380777109d60f80
joshuaroytino <joshuaroytino@gmail.com>
2022-08-18 21:31:16 +0800

    Automatically include the cookies

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 createApp(App).use(router).mount("#app");
