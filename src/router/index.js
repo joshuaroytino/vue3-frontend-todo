c0c273924dd5dd88c998f95267f7fa0daa810e9d
joshuaroytino <joshuaroytino@gmail.com>
2022-08-24 19:51:47 +0800

    Define the public pages and check if currently logged in and is in a public page, redirect to the home page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "@/views/HomeView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import authRoutes from "./auth.route";
 import todosRoute from "./todos.route";
+import { useAuthStore } from "@/stores/auth.store";
 
 const routes = [
   {
@@ -18,6 +19,20 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach(async (to) => {
+  const publicPages = ["/auth/login"];
+  const isPublicPage = publicPages.includes(to.path);
+  const authStore = useAuthStore();
+
+  if (isPublicPage && authStore.isLoggedIn) {
+    return "/todos";
+  }
+
+  if (!isPublicPage && !authStore.isLoggedIn) {
+    return "/auth/login";
+  }
 });
 
 export default router;
