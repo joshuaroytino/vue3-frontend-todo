715b6da7ecd61d1a1c18695307ebf07472a5c97c
joshuaroytino <joshuaroytino@gmail.com>
2022-09-03 18:11:35 +0800

    Add a 404 page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,3 +1,15 @@
 <template>
-  <p>Not Found</p>
+  <div
+    class="relative flex min-h-screen flex-col items-center justify-center overflow-hidden py-6 sm:py-12 bg-white"
+  >
+    <div class="max-w-xl px-5 text-center">
+      <h2 class="mb-2 text-[42px] font-bold text-zinc-800">404</h2>
+      <p class="mb-2 text-lg text-zinc-500">Page not found.</p>
+      <router-link
+        to="/"
+        class="mt-3 inline-block w-96 rounded bg-purple-600 px-5 py-3 font-medium text-white shadow-md shadow-purple-500/20 hover:bg-purple-700"
+        >Go back to Home</router-link
+      >
+    </div>
+  </div>
 </template>
